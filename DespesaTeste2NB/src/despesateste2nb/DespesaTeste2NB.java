9639c2afa393f38d82215f3855e184fee567ddcb
Karlos <Karlos@Karlos-NoteBook>
2013-03-14 22:15:07 +0000

    Projecto Incial git

---
+++ DespesaTeste2NB/src/despesateste2nb/DespesaTeste2NB.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package despesateste2nb;
+
+/**
+ *
+ * @author Karlos
+ */
+public class DespesaTeste2NB {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Teste de repositorio
+    }
+}
