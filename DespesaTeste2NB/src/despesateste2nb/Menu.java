3cee2788ca413bf53e603b2251f38657d1516208
Karlos <Karlos@Karlos-NoteBook>
2013-03-15 20:12:06 +0000

    Criação de menu (incompleto)

--- DespesaTeste2NB/src/despesateste2nb/Menu.java
+++ DespesaTeste2NB/src/despesateste2nb/Menu.java
@@ -4,10 +4,32 @@
  */
 package despesateste2nb;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Karlos
  */
 public class Menu {
+
     
+    public static Scanner ler = new Scanner(System.in);
+
+    public static void main(String[] args){
+
+        int op;
+        System.out.println("=MENU DE OPÇÕES DO PROGRAMA=");
+
+        System.out.println("1 - Criar Despesa");
+        System.out.println("2 - Criar Tipo Despesa");
+        System.out.println("3 - .....");
+        System.out.println("4 - Resgistar Despesa");
+        
+
+        System.out.println("Qual a sua opção? ");
+        op = ler.nextInt();
+        ler.nextLine();
+
+   
+    }
 }
